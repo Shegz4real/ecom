40393c8037cb06db27e429e7d342565428cf8a7f
shegz bedimma <sgonzwak@gmail.com>
2023-06-23 14:32:40 +0100

    connected mongoDB

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const connectDB = require(`./models/connect_db`);
 const dotenv = require("dotenv");
+const bodyParser = require('body-parser');
+const { default: mongoose } = require('mongoose');
 
 const app = express()
+dotenv.config(); 
 
-dotenv.config();git
+app.use(bodyParser.json)
+
 
 app.get('',(req, res)=>{
-    res.send("app working")
-})
+    res.send(`landing page`);
+});
+
+connectDB();
+mongoose.connection.once(`open`, ()=>{
+    console.log('db connected');
+    app.listen(process.env.PORT || 5000, ()=>{
+        console.log(`server is running on ${process.env.PORT}`);
+    });
+});
+
+/*INCLUDE {
+    "start":"nodemon app.js" ............ in paclage.json
+}
+*/
